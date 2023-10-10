34a88dad0b7b37b69186b5fc7e5c26df55e11a60
Jan Swiecioch <90185171+Swiecioch@users.noreply.github.com>
2023-10-10 08:07:17 +0200

    Add files via upload

---
+++ 26.09/L2/index.mjs
@@ -0,0 +1,20 @@
+import http from 'http'
+import {readFile} from 'fs/promises'
+
+const port = 3000
+const hostname = '127.0.0.1'
+
+const server = http.createServer(async (req,res)=>{
+    const filepath = './index.html'
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+
+    const content = await readFile(filepath)
+    res.end(content, 'utf-8')
+})
+
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server listening: http://${hostname}:${port}`)
+})
